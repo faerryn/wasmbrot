928a18de7faf322fba7b04d57654fb0e9d1af635
Jiang Meng Liao <alexandre.liao@gmail.com>
2020-03-11 18:37:19 -0400

    Click click click!

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 "use strict";
 
-const workerLen = window.navigator.hardwareConcurrency;
-
-const stepSize = 1;
-const scale = 2.0;
-const center_x = 0.0;
-const center_y = 0.0;
-
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-const pixelSize = (scale * 2) / Math.min(width, height);
+const zoom = 5;
 
-const left = center_x - (pixelSize * width) / 2;
-const right = center_x + (width * pixelSize) / 2;
+const overlay = document.getElementById("overlay");
+overlay.width = width;
+overlay.height = height;
+const overlayCtx = overlay.getContext("2d");
 
-const top = center_y + (pixelSize * height) / 2;
-const down = center_y - (height * pixelSize) / 2;
+const overlayWidth = width / zoom;
+const overlayHeight = height / zoom;
+
+const workerLen = Math.max(window.navigator.hardwareConcurrency - 1, 1);
+
+const stepSize = 1;
+
+function View(x, y, scale) {
+  this.x = x;
+  this.y = y;
+  this.scale = scale;
+
+  this.pixelSize = (scale * 2) / Math.min(width, height);
+
+  this.left = x - (this.pixelSize * width) / 2;
+  this.top = y + (this.pixelSize * height) / 2;
+}
+
+let view = new View(0.0, 0.0, 2.0);
+
+let canvases = [];
+let workers = [];
+let notReady = workerLen;
 
 for (let i = 0; i < workerLen; i += 1) {
   const canvas = document.createElement("canvas");
@@ -26,28 +42,98 @@ for (let i = 0; i < workerLen; i += 1) {
     Math.round((height / workerLen) * (i + 1)) -
     Math.round((height / workerLen) * i);
 
-  console.log(canvasHeight);
-
   canvas.width = width;
   canvas.height = canvasHeight;
 
   const offscreen = canvas.transferControlToOffscreen();
-
-  const workerTop = top - Math.round((height / workerLen) * i) * pixelSize;
+  canvases.push(offscreen);
 
   const worker = new Worker("worker.js", { type: "module" });
+  workers.push(worker);
+
   worker.onmessage = function(msg) {
     if (msg.data === "Ready") {
-      worker.postMessage(
-        {
-          canvas: offscreen,
-          left,
-          top: workerTop,
-          pixelSize,
-          stepSize
-        },
-        [offscreen]
-      );
+      notReady -= 1;
+      if (notReady === 0) {
+        setup();
+      }
     }
   };
+}
+
+function setup() {
+  for (let i = 0; i < workerLen; i += 1) {
+    const canvas = canvases[i];
+    const worker = workers[i];
+
+    worker.postMessage(
+      {
+        canvas,
+        left: view.left,
+        top: view.top - Math.round((height / workerLen) * i) * view.pixelSize,
+        pixelSize: view.pixelSize,
+        stepSize
+      },
+      [canvas]
+    );
+  }
+}
+
+function reset() {
+  for (let i = 0; i < workerLen; i += 1) {
+    const canvas = canvases[i];
+    const worker = workers[i];
+
+    worker.postMessage("Stop");
+
+    worker.postMessage({
+      left: view.left,
+      top: view.top - Math.round((height / workerLen) * i) * view.pixelSize,
+      pixelSize: view.pixelSize,
+      stepSize
+    });
+  }
+}
+
+window.onclick = function(e) {
+  console.log(e);
+
+  if (notReady > 0) {
+    console.log(`${notReady} workers at not ready yet`);
+  }
+
+  const row = clamp(e.clientX, overlayWidth / 2, width - overlayWidth / 2);
+  const col = clamp(e.clientY, overlayHeight / 2, height - overlayHeight / 2);
+
+  const x = view.left + row * view.pixelSize;
+  const y = view.top - col * view.pixelSize;
+  view = new View(x, y, view.scale / zoom);
+
+  console.log(view);
+
+  // send a message to the workers to reset
+  reset();
+};
+
+let mouseX = 0;
+let mouseY = 0;
+
+window.onmousemove = function(e) {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+  overlayCtx.clearRect(0, 0, width, height);
+
+  const row = clamp(mouseX, overlayWidth / 2, width - overlayWidth / 2);
+  const col = clamp(mouseY, overlayHeight / 2, height - overlayHeight / 2);
+
+  const left = row - overlayWidth / 2;
+  const top = col - overlayHeight / 2;
+
+  overlayCtx.strokeStyle = "white";
+
+  overlayCtx.strokeRect(left, top, overlayWidth, overlayHeight);
+};
+
+function clamp(x, min, max) {
+  return Math.min(Math.max(x, min), max);
 }
--- worker.js
+++ worker.js
@@ -6,7 +6,12 @@ onmessage = function(msg) {
 
 import init, { Wasmbrot } from "./wasmbrot.js";
 
-let memory;
+let memory = null;
+let canvas = null;
+let setup = false;
+let ctx;
+let wasmbrot;
+let image;
 
 async function run() {
   const wasm = await init();
@@ -17,33 +22,46 @@ async function run() {
 run();
 
 onmessage = function(msg) {
-  const canvas = msg.data.canvas;
-  const left = msg.data.left;
-  const top = msg.data.top;
-  const pixelSize = msg.data.pixelSize;
-  const stepSize = msg.data.stepSize;
+  if (memory === null) {
+    console.log("Worker not ready yet!");
+  } else {
+    const left = msg.data.left;
+    const top = msg.data.top;
+    const pixelSize = msg.data.pixelSize;
+    const stepSize = msg.data.stepSize;
 
-  const width = canvas.width;
-  const height = canvas.height;
-  const ctx = canvas.getContext("2d");
+    if (!setup) {
+      canvas = msg.data.canvas;
+      ctx = canvas.getContext("2d");
+    }
 
-  const wasmbrot = Wasmbrot.bounds(width, height, left, top, pixelSize);
+    const width = canvas.width;
+    const height = canvas.height;
 
-  const colorsPtr = wasmbrot.colors();
-  const colors = new Uint8ClampedArray(
-    memory.buffer,
-    colorsPtr,
-    4 * width * height
-  );
-  const image = new ImageData(colors, width);
+    wasmbrot = Wasmbrot.bounds(width, height, left, top, pixelSize);
+    const colorsPtr = wasmbrot.colors();
+    const colors = new Uint8ClampedArray(
+      memory.buffer,
+      colorsPtr,
+      4 * width * height
+    );
+    image = new ImageData(colors, width);
 
-  function draw() {
-    wasmbrot.step(stepSize);
-    wasmbrot.colorize();
+    if (!setup) {
+      function draw() {
+        wasmbrot.step(stepSize);
+        wasmbrot.colorize();
 
-    ctx.putImageData(image, 0, 0);
-    requestAnimationFrame(draw);
+        ctx.putImageData(image, 0, 0);
+
+        setTimeout(function() {
+          requestAnimationFrame(draw);
+        }, wasmbrot.depth() / 128);
+      }
+
+      requestAnimationFrame(draw);
+    }
+
+    setup = true;
   }
-
-  requestAnimationFrame(draw);
 };
